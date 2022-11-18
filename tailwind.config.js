b3a6c19556cf5889e859abf334660c65639eab71
frank-chris <chrisfrancischris@gmail.com>
2022-11-18 14:19:51 -0800

    Full update part-1

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'sans': ['Inter var'],
+    }
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#E6790B",
+          secondary: "#3F96B3",
+          accent: "#60220F",
+          neutral: "#032a3d",
+          "base-100": "#fceddb",
+          
+          "--rounded-box": "0.5rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0.25rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
+          "--animation-btn": "0.25s", // duration of animation when you click on button
+          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-text-case": "uppercase", // set default text transform for buttons
+          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--border-btn": "1px", // border width of buttons
+          "--tab-border": "1px", // border width of tabs
+          "--tab-radius": "0.5rem", // border radius of tabs
+        },
+      },
+    ],
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
